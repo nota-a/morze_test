7eb492608aec3ffe8702ff0a65eebdbd7df3b9d5
nota-a <romanichKslp@mail.ru>
2025-04-11 13:58:01 +0400

    Add files via upload

---
+++ Objects/mdr32f9qx_rst_clk.d
@@ -0,0 +1,16 @@
+./objects/mdr32f9qx_rst_clk.o: \
+  milandr-standard-peripherals-library\src\MDR32F9Qx_StdPeriph_Driver\src\MDR32F9Qx_rst_clk.c \
+  RTE\Device\MDR1986BE92\MDR32F9Qx_config.h \
+  C:\Users\ADM\AppData\Local\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  RTE\_Target_1\RTE_Components.h \
+  C:\Users\ADM\AppData\Local\Arm\Packs\Keil\MDR1986BExx\2.0.3\Libraries\CMSIS\MDR32Fx\DeviceSupport\MDR1986VE9x\inc\MDR32Fx.h \
+  C:\Users\ADM\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm3.h \
+  C:\Users\ADM\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\ADM\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\ADM\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Users\ADM\AppData\Local\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  C:\Users\ADM\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\.\m-profile\cmsis_armclang_m.h \
+  C:\Users\ADM\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\m-profile\armv7m_mpu.h \
+  RTE\Device\MDR1986BE92\system_MDR32F9Qx.h \
+  milandr-standard-peripherals-library\src\MDR32F9Qx_StdPeriph_Driver\inc\MDR32F9Qx_rst_clk.h \
+  milandr-standard-peripherals-library\src\MDR32F9Qx_StdPeriph_Driver\inc\MDR32F9Qx_lib.h
---
+++ Objects/system_mdr32f9qx.d
@@ -0,0 +1,13 @@
+./objects/system_mdr32f9qx.o: RTE\Device\MDR1986BE92\system_MDR32F9Qx.c \
+  C:\Users\ADM\AppData\Local\Arm\Packs\Keil\MDR1986BExx\2.0.3\Libraries\CMSIS\MDR32Fx\DeviceSupport\MDR1986VE9x\inc\MDR32Fx.h \
+  RTE\Device\MDR1986BE92\MDR32F9Qx_config.h \
+  C:\Users\ADM\AppData\Local\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  RTE\_Target_1\RTE_Components.h \
+  C:\Users\ADM\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm3.h \
+  C:\Users\ADM\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\ADM\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\ADM\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Users\ADM\AppData\Local\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  C:\Users\ADM\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\.\m-profile\cmsis_armclang_m.h \
+  C:\Users\ADM\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\m-profile\armv7m_mpu.h \
+  RTE\Device\MDR1986BE92\system_MDR32F9Qx.h
