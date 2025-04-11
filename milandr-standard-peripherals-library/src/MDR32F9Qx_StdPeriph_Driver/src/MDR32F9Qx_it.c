e46e3ba088c9a21e7da117afdcd47657e30f4d17
nota-a <romanichKslp@mail.ru>
2025-04-11 14:10:56 +0400

    Add files via upload

---
+++ milandr-standard-peripherals-library/src/MDR32F9Qx_StdPeriph_Driver/src/MDR32F9Qx_it.c
@@ -0,0 +1,443 @@
+/**
+  ******************************************************************************
+  * @file    Examples/MDR32F9Q3_EVAL/CAN/LoopBack_RTR/MDR32F9Qx_it.c
+  * @author  Milandr Application Team
+  * @version V1.2.0
+  * @date    12/09/2011
+  * @brief   Main Interrupt Service Routines.
+  *
+  ******************************************************************************
+  * <br><br>
+  *
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, MILANDR SHALL NOT BE HELD LIABLE FOR ANY DIRECT, INDIRECT
+  * OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+  *
+  * <h2><center>&copy; COPYRIGHT 2011 Milandr</center></h2>
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "MDR32F9Qx_it.h"
+#include "MDR32F9Qx_can.h"
+#include "MDR32F9Qx_port.h"
+#include "MDR32F9Qx_timer.h"
+#include "MDR32F9Qx_uart.h"
+
+
+#define LED_RED_ON 		PORT_WriteBit(MDR_PORTE, PORT_Pin_6, 1)
+#define LED_RED_OFF 	PORT_WriteBit(MDR_PORTE, PORT_Pin_6, 0)
+
+
+
+
+#define sec_180 		180
+
+int Count_t1;
+int invert;
+uint32_t rx_data;
+uint32_t rx_data2;
+
+
+
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+extern __IO uint32_t ret;
+extern __IO uint32_t rx_buf;
+extern int ADDR_ID;
+
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+
+/*******************************************************************************
+* Function Name  : NMI_Handler
+* Description    : This function handles NMI exception.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void NMI_Handler(void)
+{
+}
+/*******************************************************************************
+* Function Name  : HardFault_Handler
+* Description    : This function handles Hard Fault exception.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void HardFault_Handler(void)
+{
+  /* Go to infinite loop when Hard Fault exception occurs */
+  while (1)
+  {
+  }
+}
+/*******************************************************************************
+* Function Name  : MemManage_Handler
+* Description    : This function handles Memory Manage exception.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void MemManage_Handler(void)
+{
+  /* Go to infinite loop when Memory Manage exception occurs */
+  while (1)
+  {
+  }
+}
+/*******************************************************************************
+* Function Name  : BusFault_Handler
+* Description    : This function handles Bus Fault exception.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void BusFault_Handler(void)
+{
+  /* Go to infinite loop when Bus Fault exception occurs */
+  while (1)
+  {
+
+  }
+}
+/*******************************************************************************
+* Function Name  : UsageFault_Handler
+* Description    : This function handles Usage Fault exception.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void UsageFault_Handler(void)
+{
+  /* Go to infinite loop when Usage Fault exception occurs */
+  while (1)
+  {
+  }
+}
+/*******************************************************************************
+* Function Name  : SVC_Handler
+* Description    : This function handles SVCall exception.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void SVC_Handler(void)
+{
+}
+/*******************************************************************************
+* Function Name  : DebugMon_Handler
+* Description    : This function handles Debug Monitor exception.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void DebugMon_Handler(void)
+{
+}
+/*******************************************************************************
+* Function Name  : PendSV_Handler
+* Description    : This function handles Debug PendSV exception.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void PendSV_Handler(void)
+{
+}
+/*******************************************************************************
+* Function Name  : SysTick_Handler
+* Description    : This function handles SysTick Handler.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void SysTick_Handler(void)
+{
+}
+/*******************************************************************************
+* Function Name  : CAN1_IRQHandler
+* Description    : This function handles CAN1 global interrupt request.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void CAN1_IRQHandler(void)
+{
+	
+	
+  CAN_RxMsgTypeDef RxMessage;
+
+  CAN_GetRawReceivedData(MDR_CAN1, rx_buf, &RxMessage);
+
+
+  CAN_ITClearRxTxPendingBit(MDR_CAN1, rx_buf, CAN_STATUS_RX_READY);   //сброс флага прерывания
+
+  if (RxMessage.Rx_Header.ID == ADDR_ID)
+  {
+		rx_data = RxMessage.Data[0];
+		if (((RxMessage.Data[1] >> 31) & 0x1) == 1)
+		{
+			ret = 2;		// режим отладки (технологии)
+		}
+		else
+		{
+			ret = 1;		// рабочий режим
+		}
+  }
+  else
+  {
+    if (RxMessage.Rx_Header.ID == ADDR_ID+1)
+    {
+    	rx_data = RxMessage.Data[0];
+    	rx_data2 = RxMessage.Data[1];
+    	ret = 3;						//режим калибровки
+    }
+    else
+    {
+    	ret = 1;
+    }
+  }
+
+}
+/*******************************************************************************
+* Function Name  : CAN2_IRQHandler
+* Description    : This function handles CAN2 global interrupt request.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void CAN2_IRQHandler(void)
+{
+}
+/*******************************************************************************
+* Function Name  : USB_IRQHandler
+* Description    : This function handles USB global interrupt request.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void USB_IRQHandler(void)
+{
+}
+/*******************************************************************************
+* Function Name  : UART1_IRQHandler
+* Description    : This function handles UART1 global interrupt request.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void UART1_IRQHandler(void)
+{
+//	  if (UART_GetITStatusMasked(MDR_UART1, UART_IT_RX) == SET)
+//	  {
+//	    UART_ClearITPendingBit(MDR_UART1, UART_IT_RX);
+////	    rx_data = UART_read();
+//	    ret = 1;
+//	  }
+}
+/*******************************************************************************
+* Function Name  : UART2_IRQHandler
+* Description    : This function handles UART2 global interrupt request.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void UART2_IRQHandler(void)
+{
+}
+/*******************************************************************************
+* Function Name  : SSP1_IRQHandler
+* Description    : This function handles SSP1 global interrupt request.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void SSP1_IRQHandler(void)
+{
+}
+/*******************************************************************************
+* Function Name  : I2C_IRQHandler
+* Description    : This function handles I2C global interrupt request.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void I2C_IRQHandler(void)
+{
+}
+/*******************************************************************************
+* Function Name  : POWER_IRQHandler
+* Description    : This function handles POWER global interrupt request.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void POWER_IRQHandler(void)
+{
+}
+/*******************************************************************************
+* Function Name  : WWDG_IRQHandler
+* Description    : This function handles WWDG global interrupt request.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void WWDG_IRQHandler(void)
+{
+}
+
+/*******************************************************************************
+* Function Name  : Timer1_IRQHandler
+* Description    : This function handles Timer1 global interrupt request.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void Timer1_IRQHandler(void)
+{
+
+//	uint32_t intFlag =  1 << TIMER_IE_CNT_ARR_EVENT_IE_Pos;
+//	TIMER_ClearFlag(MDR_TIMER1, intFlag);
+//	TIMER_ClearFlag(MDR_TIMER1, TIMER_STATUS_CNT_ZERO | TIMER_STATUS_CNT_ARR);
+//	if (TIMER_GetStatus(MDR_TIMER1)||TIMER_STATUS_CNT_ARR_EVENT)
+//	{
+//		Count_t1++;
+//		if (Count_t1==sec_180)
+//			{
+//			//Count_t1 = 0;
+//			/*if (invert!=1)
+//			{
+//				invert = 1;
+//				LED_RED_ON;
+//			}
+//			else
+//			{
+//				invert = 0;
+//				LED_RED_OFF;
+//			}*/
+//			TIMER_Cmd(MDR_TIMER1, DISABLE);
+//			LED_RED_ON;
+
+//			}
+//		else
+//		{
+//			LED_RED_OFF;
+//		}
+
+
+//	}
+
+
+}
+/*******************************************************************************
+* Function Name  : Timer2_IRQHandler
+* Description    : This function handles Timer2 global interrupt request.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void Timer2_IRQHandler(void)
+{
+}
+/*******************************************************************************
+* Function Name  : Timer3_IRQHandler
+* Description    : This function handles Timer3 global interrupt request.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void Timer3_IRQHandler(void)
+{
+}
+/*******************************************************************************
+* Function Name  : ADC_IRQHandler
+* Description    : This function handles ADC global interrupt request.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void ADC_IRQHandler(void)
+{
+}
+/*******************************************************************************
+* Function Name  : COMPARATOR_IRQHandler
+* Description    : This function handles COMPARATOR global interrupt request.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void COMPARATOR_IRQHandler(void)
+{
+}
+/*******************************************************************************
+* Function Name  : SSP2_IRQHandler
+* Description    : This function handles SSP2 global interrupt request.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void SSP2_IRQHandler(void)
+{
+}
+/*******************************************************************************
+* Function Name  : BACKUP_IRQHandler
+* Description    : This function handles BACKUP global interrupt request.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void BACKUP_IRQHandler(void)
+{
+}
+/*******************************************************************************
+* Function Name  : EXT_INT1_IRQHandler
+* Description    : This function handles EXT_INT1 interrupt request.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void EXT_INT1_IRQHandler(void)
+{
+}
+/*******************************************************************************
+* Function Name  : EXT_INT2_IRQHandler
+* Description    : This function handles EXT_INT2 interrupt request.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void EXT_INT2_IRQHandler(void)
+{
+}
+/*******************************************************************************
+* Function Name  : EXT_INT3_IRQHandler
+* Description    : This function handles EXT_INT3 global interrupt request.
+*                  requests.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void EXT_INT3_IRQHandler(void)
+{
+}
+/*******************************************************************************
+* Function Name  : EXT_INT4_IRQHandler
+* Description    : This function handles EXT_INT4 interrupt request.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void EXT_INT4_IRQHandler(void)
+{
+}
+
+/******************* (C) COPYRIGHT 2011 Milandr *********/
+
+/* END OF FILE MDR32F9Qx_it.c */
